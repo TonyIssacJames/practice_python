039294a7f2d9d86de6ca309b292fedac59482fd6
Tony Issac James <tonyissacjames@gmail.com>
2019-03-26 00:27:48 +0530

    first day excercises

---
+++ day1/readme.md
@@ -0,0 +1,6 @@
+## Commands ran
+
+gcc sample.c
+gcc -E sample.c
+gcc -S sample.c -o sample.asm
+gcc -S sample.c -o smaple_opt.asm
---
+++ day1/sample.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 100;
+	int b = 200;
+	int c = a * b;
+
+	printf("c = %d\n",c);
+
+	return 0;
+
+}
